14fc62f3e057f8dc541bc1104abff4af047d647a
preyelg <geeftedpl@gmail.com>
2025-05-25 17:57:20 -0500

    terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,11 +2,6 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
-  sensitive = true
-}
-
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
