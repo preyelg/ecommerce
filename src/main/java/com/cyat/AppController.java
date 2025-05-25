629731a7604cec9ad794f185fe7cd611e884b343
preyelg <geeftedpl@gmail.com>
2025-05-25 15:20:22 -0500

    initial

---
+++ src/main/java/com/cyat/AppController.java
@@ -0,0 +1,13 @@
+package com.cyat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot E-Commerce App!";
+    }
+}
