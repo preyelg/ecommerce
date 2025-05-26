e42dd7ed7da2f0eb53a25c69455d593a7d71ed69
preyelg <geeftedpl@gmail.com>
2025-05-25 22:54:18 -0500

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,7 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
   vpc_id     = module.vpc.vpc_id
 
   enable_irsa = true
