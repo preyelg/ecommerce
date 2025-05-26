1844105d133cc449387c121dad2a1464419a9164
preyelg <geeftedpl@gmail.com>
2025-05-25 19:19:37 -0500

    terraform

---
+++ terraform/aws-auth.tf
@@ -0,0 +1,20 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapUsers = yamlencode([
+      {
+        userarn  = "arn:aws:iam::207567759296:user/preye_aws"
+        username = "preye_aws"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+}
