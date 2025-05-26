68adbaf1d28cc0ee8d3bfe551a9d6e2615170a3a
preyelg <geeftedpl@gmail.com>
2025-05-25 23:43:05 -0500

    initial

--- terraform/aws-auth.tf
+++ terraform/aws-auth.tf
@@ -1,3 +1,17 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
+
 resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name      = "aws-auth"
